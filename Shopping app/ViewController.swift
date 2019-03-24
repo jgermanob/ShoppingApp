e864f5bd24d799ebdd6b8c77a40783a2a3c78315
Jesús Germán Ortiz Barajas <jgerman@macbook-pro-de-jesus.local>
2019-03-24 14:58:08 -0600

    Initial Commit

---
+++ Shopping app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shopping app
+//
+//  Created by Jesús Germán Ortiz Barajas on 3/24/19.
+//  Copyright © 2019 germanco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
